0e8db5a6c93898668a84cc220ce7a5302be7f327
Enfield-Li <1076462153@qq.com>
2022-04-18 10:08:30 +0800

    Entity and table

--- src/main/java/com/example/demo/Student/Student.java
+++ src/main/java/com/example/demo/Student/Student.java
@@ -1,6 +1,28 @@
 package com.example.demo.Student;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Student {
+    // Set up sequence
+    @Id
+    @SequenceGenerator(
+        name = "student_sequence",
+        sequenceName = "student_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+    )
+    
+    
     private Long id;
     private String name;
     private String email;
