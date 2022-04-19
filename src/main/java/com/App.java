4e5b7b4e6ef7a5ca8ee97b2281cbea2aaf785f4f
Enfield-Li <1076462153@qq.com>
2022-04-19 16:54:59 +0800

    test done

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -5,6 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
+import com.stream.Stream;
+import com.stream.StreamRepository;
+import com.viewer.Viewer;
+import com.viewer.ViewerRepository;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,5 +25,27 @@ public class App {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext configContext = SpringApplication.run(App.class, args);
 
+		StreamRepository streamRepo = configContext.getBean(StreamRepository.class);
+		ViewerRepository viewerRepo = configContext.getBean(ViewerRepository.class);
+
+		Stream newStream1 = new Stream("stream1");
+		Stream newStream2 = new Stream("stream2");
+		Stream newStream3 = new Stream("stream3");
+		List<Stream> streamList = Arrays.asList(newStream1, newStream2, newStream3);
+
+		Viewer newViewer1 = new Viewer("viewer1");
+		Viewer newViewer2 = new Viewer("viewer2");
+		Viewer newViewer3 = new Viewer("viewer3");
+		List<Viewer> viewerList = Arrays.asList(newViewer1, newViewer2, newViewer3);
+
+		streamRepo.saveAll(streamList);
+
+		newViewer1.followedStream(newStream1);
+		newViewer1.followedStream(newStream2);
+
+		newViewer3.followedStream(newStream3);
+		newViewer3.followedStream(newStream2);
+
+		viewerRepo.saveAll(viewerList);
 	}
 }
