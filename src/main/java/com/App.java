d42450e473d3eb95ae1ce95a3387287a1903cd28
Enfield-Li <1076462153@qq.com>
2022-04-25 11:25:00 +0800

    done

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -19,11 +19,44 @@ public class App {
       args
     );
 
-    PersonRepository personRepo = configContext.getBean(PersonRepository.class);
+    // String = a reference data type that can store one or more characters
+    //          reference data types have access to useful methods
 
-	Person person = new Person("fName", "lName", "emailhere", 20);
+    //boolean result = name.equalsIgnoreCase("bro");
+    //int result = name.length();
+    //char result = name.charAt(0);
+    //int result = name.indexOf("o");
+    //boolean result = name.isEmpty();
+    //String result = name.toUpperCase();
+    //String result = name.toLowerCase();
+    //String result = name.trim();
+    //String result = name.replace('o', 'a');
 
-	person.eat();
-	System.out.println(person.firstName);
+    // *** wrapper class ***
+
+    //	   provides a way to use primitive data types as reference data types
+    //     reference data types contain useful methods
+    //     can be used with collections (ex.ArrayList)
+
+    // primitive  //wrapper
+    // ---------  //-------
+    // boolean    Boolean
+    // char       Character
+    // int        Integer
+    // double     Double
+
+    // autoboxing = the automatic conversion that
+    //            	the Java compiler makes between the primitive types
+    //            	and their corresponding object wrapper classes
+
+    // unboxing = the reverse of autoboxing. Automatic conversion of wrapper class to primitive
+
+    Boolean a = true;
+    Character b = '@';
+    Integer c = 123;
+    Double d = 3.14;
+    String str = "bro";
+
+    if (a == true) System.out.println("ture!");
   }
 }
