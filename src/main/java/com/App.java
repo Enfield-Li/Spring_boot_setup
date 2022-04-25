13a91f798ff1c2ca43422b035c7084fa076f6fd3
Enfield-Li <1076462153@qq.com>
2022-04-25 17:36:48 +0800

    done

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -2,6 +2,7 @@ package com;
 
 import com.person.Person;
 import com.person.PersonRepository;
+import java.util.HashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -19,11 +20,30 @@ public class App {
       args
     );
 
-    PersonRepository personRepo = configContext.getBean(PersonRepository.class);
+    // PersonRepository personRepo = configContext.getBean(PersonRepository.class);
 
-	Person person = new Person("fName", "lName", "emailhere", 20);
+    // Person person = new Person("fName", "lName", "emailhere", 20);
 
-	person.eat();
-	System.out.println(person.firstName);
+    // person.eat();
+    // System.out.println(person.firstName);
+
+    HashMap<String, String> hMap = new HashMap<>();
+
+    hMap.put("personOne", "name1");
+    hMap.putIfAbsent("personThree", "name3");
+    hMap.put("personTwo", "name2");
+
+    // hMap.putIfAbsent("personTwo", "name222");
+
+    hMap.replace("personTwo", "name222");
+    // hMap.remove("personThree");
+
+    // System.out.println(hMap);
+    // System.out.println(hMap.get("personOne"));
+
+    for (String i : hMap.keySet()) {
+      System.out.println(i);
+      System.out.println(hMap.get(i));
+    }
   }
 }
