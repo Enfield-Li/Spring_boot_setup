499d75569c1283a1826b007a2faed5f2930255d2
Enfield-Li <1076462153@qq.com>
2022-04-25 09:54:40 +0800

    .

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.ioc.Airtel;
+import com.ioc.Sim;
+import com.ioc.Vodaphone;
 import com.person.PersonRepository;
 
 import org.slf4j.Logger;
@@ -12,12 +15,21 @@ import org.slf4j.LoggerFactory;
 @SpringBootApplication
 public class App {
 
-	private static final Logger log = LoggerFactory.getLogger(App.class);
+	// private static final Logger log = LoggerFactory.getLogger(App.class);
 	
 	public static void main(String[] args) {
 		ConfigurableApplicationContext configContext = SpringApplication.run(App.class, args);
+		// PersonRepository personRepo = configContext.getBean(PersonRepository.class);
 
-		PersonRepository personRepo = configContext.getBean(PersonRepository.class);
+		// Airtel airtel = new Airtel();
+		// airtel.calling();
+		// airtel.data();
+
+		// interface reference
+		Sim sim = new Airtel();
+
+		sim.calling();
+		sim.data();
 		
 
 	}
