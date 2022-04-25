3cc1d1157a31d3b1a8fb59ebaaf846b23cac9454
Enfield-Li <1076462153@qq.com>
2022-04-25 10:46:38 +0800

    done

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,24 +1,29 @@
 package com;
 
+import com.person.Person;
+import com.person.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import com.person.PersonRepository;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 @SpringBootApplication
 public class App {
 
-	private static final Logger log = LoggerFactory.getLogger(App.class);
-	
-	public static void main(String[] args) {
-		ConfigurableApplicationContext configContext = SpringApplication.run(App.class, args);
+  private static final Logger log = LoggerFactory.getLogger(App.class);
 
-		PersonRepository personRepo = configContext.getBean(PersonRepository.class);
-		
+  public static void main(String[] args) {
+    ConfigurableApplicationContext configContext = SpringApplication.run(
+      App.class,
+      args
+    );
 
-	}
+    PersonRepository personRepo = configContext.getBean(PersonRepository.class);
+
+	Person person = new Person("fName", "lName", "emailhere", 20);
+
+	person.eat();
+	System.out.println(person.firstName);
+  }
 }
--- src/main/java/com/person/Person.java
+++ src/main/java/com/person/Person.java
@@ -10,40 +10,39 @@ import javax.persistence.UniqueConstraint;
 
 @Entity(name = "Person")
 @Table(
-    name = "person",
-    uniqueConstraints = {
-        @UniqueConstraint(columnNames = "email", name = "person_email_unique")
-    }
+  name = "person",
+  uniqueConstraints = {
+    @UniqueConstraint(columnNames = "email", name = "person_email_unique"),
+  }
 )
 public class Person {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    
-    @Column(
-        name = "first_name",
-        nullable = false,
-        columnDefinition = "TEXT"
-    )
-    private String firstName;
 
-    @Column(
-        name = "last_name",
-        nullable = false,
-        columnDefinition = "TEXT"
-    )
-    private String lastName;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
-    @Column(
-        name = "email",
-        nullable = false,
-        columnDefinition = "TEXT"
-    )
-    private String email;
+  @Column(name = "first_name", nullable = false, columnDefinition = "TEXT")
+  public String firstName;
 
-    @Column(
-        name = "age",
-        nullable = true
-    )
-    private Integer age;
+  @Column(name = "last_name", nullable = false, columnDefinition = "TEXT")
+  private String lastName;
+
+  @Column(name = "email", nullable = false, columnDefinition = "TEXT")
+  private String email;
+
+  @Column(name = "age", nullable = true)
+  private Integer age;
+
+  public Person() {}
+
+  public Person(String firstName, String lastName, String email, Integer age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.age = age;
+  }
+
+  public void eat() {
+    System.out.println(this.lastName + " is eating");
+  }
 }
