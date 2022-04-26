4ef5920259b63d785d97eee4e76ec54d24fe63e6
Enfield-Li <1076462153@qq.com>
2022-04-26 17:08:11 +0800

    done

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -9,7 +9,6 @@ import org.apache.catalina.core.ApplicationContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
--- src/main/java/com/shape/Triangle.java
+++ src/main/java/com/shape/Triangle.java
@@ -1,12 +1,30 @@
 package com.shape;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Triangle implements Shape {
 
   private String type;
+  private Integer height;
 
-  @Override
-  public void draw() {
-    System.out.println("draw a triangle and type is " + getType());
+  // No args constructor for spring reflect api
+  public Triangle() {}
+
+  public Triangle(String type) {
+    this.type = type;
+  }
+
+  public Triangle(Integer height) {
+    this.height = height;
+  }
+
+  public Triangle(String type, Integer height) {
+    this.type = type;
+    this.height = height;
+  }
+
+  public Integer getHeight() {
+    return this.height;
   }
 
   public String getType() {
@@ -14,7 +32,14 @@ public class Triangle implements Shape {
   }
 
   // setter injection
-  public void setType(String type) {
-    this.type = type;
+  // public void setType(String type) {
+  //   this.type = type;
+  // }
+
+  @Override
+  public void draw() {
+    System.out.println(
+      "draw a triangle and type is " + getType() + " of height: " + getHeight()
+    );
   }
 }
