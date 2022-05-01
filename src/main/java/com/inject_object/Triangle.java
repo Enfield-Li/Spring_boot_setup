fc9fc61e6e95f8d9c64682181869a7793fe4246c
Enfield-Li <88321505+Enfield1234@users.noreply.github.com>
2022-05-01 11:44:58 +0800

    done

--- src/main/java/com/inject_object/Triangle.java
+++ src/main/java/com/inject_object/Triangle.java
@@ -1,34 +1,59 @@
 package com.inject_object;
 
-import java.util.List;
 import java.util.Objects;
 
 public class Triangle {
 
-  private List<Point> points;
+  private Point pointA;
+  private Point pointB;
+  private Point pointC;
 
-  public void draw() {
-    for (Point point : points) {
-      System.out.println("Point " + point.getX() + " + " + point.getY());
-    }
-  }
+  public void draw() {}
 
   public Triangle() {}
 
-  public Triangle(List<Point> points) {
-    this.points = points;
+  public Triangle(Point pointA, Point pointB, Point pointC) {
+    this.pointA = pointA;
+    this.pointB = pointB;
+    this.pointC = pointC;
   }
 
-  public List<Point> getPoints() {
-    return this.points;
+  public Point getPointA() {
+    return this.pointA;
   }
 
-  public void setPoints(List<Point> points) {
-    this.points = points;
+  public void setPointA(Point pointA) {
+    this.pointA = pointA;
   }
 
-  public Triangle points(List<Point> points) {
-    setPoints(points);
+  public Point getPointB() {
+    return this.pointB;
+  }
+
+  public void setPointB(Point pointB) {
+    this.pointB = pointB;
+  }
+
+  public Point getPointC() {
+    return this.pointC;
+  }
+
+  public void setPointC(Point pointC) {
+    this.pointC = pointC;
+  }
+
+  public Triangle pointA(Point pointA) {
+    setPointA(pointA);
+    return this;
+  }
+
+  public Triangle pointB(Point pointB) {
+    setPointB(pointB);
+    return this;
+  }
+
+  public Triangle pointC(Point pointC) {
+    setPointC(pointC);
     return this;
   }
 
@@ -39,16 +64,32 @@ public class Triangle {
       return false;
     }
     Triangle triangle = (Triangle) o;
-    return Objects.equals(points, triangle.points);
+    return (
+      Objects.equals(pointA, triangle.pointA) &&
+      Objects.equals(pointB, triangle.pointB) &&
+      Objects.equals(pointC, triangle.pointC)
+    );
   }
 
   @Override
   public int hashCode() {
-    return Objects.hashCode(points);
+    return Objects.hash(pointA, pointB, pointC);
   }
 
   @Override
   public String toString() {
-    return "{" + " points='" + getPoints() + "'" + "}";
+    return (
+      "{" +
+      " pointA='" +
+      getPointA() +
+      "'" +
+      ", pointB='" +
+      getPointB() +
+      "'" +
+      ", pointC='" +
+      getPointC() +
+      "'" +
+      "}"
+    );
   }
 }
