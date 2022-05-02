05bdb5253be0d01916455fbfc386e2c10a15b047
Enfield-Li <88321505+Enfield1234@users.noreply.github.com>
2022-05-02 10:20:59 +0800

    1

---
+++ src/main/java/com/Exam.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Exam {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.name = "name1";
+
+        student.showName();
+    }
+    
+}
---
+++ src/main/java/com/Student.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Student {
+
+  String name;
+
+  public void showName() {
+    System.out.println(this.name);
+  }
+}
