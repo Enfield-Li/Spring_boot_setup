c47fe48e4dd3208db13f28da6658f6c36d69d5a5
Enfield-Li <1076462153@qq.com>
2022-07-01 07:28:20 +0800

    .

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -21,7 +21,7 @@ public class App {
 
   // private static final Logger log = LoggerFactory.getLogger(App.class);
   @Autowired
-  static User user2;
+  User user2;
 
   public App(User user2) {
     this.user2 = user2;
@@ -57,7 +57,6 @@ public class App {
     // without DI
     User user = configContext.getBean(User.class);
     user.add("1");
-    user2.add("2");
     // User user = new User(new PostgreSqlDatabase());
     // User user2 = new User(new MySqlDatabase());
     // user.add("new user");
